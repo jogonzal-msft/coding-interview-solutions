96ce50eff402ba3f93fc5d598105c1bf4e22e1c0
Jorge Aguirre <jogonzal@microsoft.com>
2015-06-01 00:32:56 -0700

    Create .sln and .csproj files + solve first problem!

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Chapter1/Problem1_1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace CodingInterviewSolutions.Chapter1
+{
+	/// <summary>
+	/// Implement an algorithm to determine if a string has all unique characters
+	/// What if you can not use additional data structures?
+	/// </summary>
+	public class Problem1_1
+	{
+		/// <summary>
+		/// Approach: Use a hashset to know if the characters have already been seen
+		/// Adding to hashset (O(1))
+		/// Checking hashset for value (O(1))
+		/// </summary>
+		public static bool HasOnlyUniqueCharacters(string input)
+		{
+			HashSet<char> hashset = new HashSet<char>();
+
+			foreach(var character in input)
+			{
+				if (hashset.Contains(character))
+				{
+					// This means that the character was already present in the string
+					return false;
+				}
+				hashset.Add(character);
+			}
+
+			// If we looped through the array without finding a match, done!
+			return true;
+		}
+
+		public static bool HasOnlyUniqueCharactersWithoutDataStructure(string input)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				char character = input[i];
+				// Check if from this point to the end, the character is present
+				for (int k = i + 1; k < input.Length; k++)
+				{
+					if (character == input[k])
+					{
+						// Any match would indicate characters are not unique
+						return false;
+					}
+				}
+			}
+
+			// If we looped through the array without finding a match, done!
+			return true;
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Chapter1/Problem1_1UnitTests.cs
@@ -0,0 +1,39 @@
+﻿using CodingInterviewSolutions.Chapter1;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Chapter1
+{
+	[TestClass]
+	public class Problem1_1UnitTests
+	{
+		[TestMethod]
+		public void UniqueLetterWords_AreUnique()
+		{
+			TestProblem1_1("helo", true);
+			TestProblem1_1("mynaeisro", true);
+			TestProblem1_1("thisdoen rp", true);
+			TestProblem1_1("asdfghjkl;", true);
+			TestProblem1_1(" ", true);
+			TestProblem1_1("", true);
+		}
+
+		[TestMethod]
+		public void WordsWithRepeatedLetters_AreUnique()
+		{
+			TestProblem1_1("helle", false);
+			TestProblem1_1("doggy", false);
+			TestProblem1_1("this repeats a bunch of letters", false);
+			TestProblem1_1("two space ;", false);
+		}
+
+		/// <summary>
+		/// Test both implementations of problem 1
+		/// </summary>
+		private void TestProblem1_1(string input, bool expectedResult)
+		{
+			Problem1_1.HasOnlyUniqueCharacters(input).Should().Be(expectedResult);
+			Problem1_1.HasOnlyUniqueCharactersWithoutDataStructure(input).Should().Be(expectedResult);
+		}
+	}
+}
