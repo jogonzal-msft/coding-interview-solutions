10b067022ea4820fde31865b19a2ce0d34a832c4
Jorge <jogonzal@microsoft.com>
2015-06-08 01:16:31 -0700

    Solve problem 1.2

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Chapter1/Problem1_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviewSolutions.Chapter1
+{
+	/// <summary>
+	/// Write code to reverse a C-Style String (C-String means that “abcd” is
+	/// represented as five characters, including the null character)
+	/// </summary>
+	public class Problem1_2
+	{
+		/// <summary>
+		/// Approach: SWAP characters from first to last using a StringBuilder
+		/// </summary>
+		public static string ReverseCString(string input)
+		{
+			if (input == null)
+			{
+				throw new ArgumentNullException("input");
+			}
+
+			StringBuilder sb = new StringBuilder(input);
+			for (int i = 0; i < input.Length/2; i++)
+			{
+				char charToSwap = sb[i];
+				sb[i] = input[input.Length - i - 1];
+				sb[input.Length - i - 1] = charToSwap;
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Chapter1/Problem1_2UnitTests.cs
@@ -0,0 +1,32 @@
+﻿using CodingInterviewSolutions.Chapter1;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Chapter1
+{
+	[TestClass]
+	public class Problem1_2UnitTests
+	{
+		[TestMethod]
+		public void UniqueLetterWords_AreUnique()
+		{
+			TestProblem1_2("helo", "oleh");
+			TestProblem1_2("My name is jorge", "egroj si eman yM");
+		}
+
+		[TestMethod]
+		public void Palindromes_Unchanged()
+		{
+			TestProblem1_2("cowoc", "cowoc");
+			TestProblem1_2("tacocat", "tacocat");
+		}
+
+		/// <summary>
+		/// Test problem 2
+		/// </summary>
+		private void TestProblem1_2(string input, string expectedResult)
+		{
+			Problem1_2.ReverseCString(input).Should().Be(expectedResult);
+		}
+	}
+}
