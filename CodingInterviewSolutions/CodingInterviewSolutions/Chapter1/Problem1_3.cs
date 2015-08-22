f9aed1118d9f0aacd0704faa14b50654a3a8aeab
Jorge <jogonzal@microsoft.com>
2015-08-22 13:06:38 -0700

    Solve problem 1.3

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Chapter1/Problem1_3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace CodingInterviewSolutions.Chapter1
+{
+	/// <summary>
+	/// Write code to remove duplicate characters ins  astring without using any additional buffer
+	/// </summary>
+	public class Problem1_3
+	{
+		/// <summary>
+		/// Approach 1: Use hashset to keep history of characters that we've seen - this is better but does not fit into the constraints of the problem
+		/// Approach 2 (chosen): For each character that you see, swipe the rest of the string for that character
+		/// </summary>
+		public static string RemoveDuplicateCharacters(string input)
+		{
+			if (input == null)
+			{
+				throw new ArgumentNullException("input");
+			}
+
+			StringBuilder sb = new StringBuilder(input);
+			for (int i = 0; i < sb.Length; i++)
+			{
+				char charToRemove = sb[i];
+				for(int x  = i + 1; x < sb.Length; x++)
+				{
+					if (sb[x] == charToRemove)
+					{
+						sb.Remove(x, 1);
+					}
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Chapter1/Problem1_3UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using CodingInterviewSolutions.Chapter1;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Chapter1
+{
+	[TestClass]
+	public class Problem1_3UnitTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			Problem1_3.RemoveDuplicateCharacters("doggie")
+				.Should().Be("dogie");
+	
+			Problem1_3.RemoveDuplicateCharacters("my name is")
+				.Should().Be("my naeis");
+
+			Problem1_3.RemoveDuplicateCharacters("Superman")
+				.Should().Be("Superman");
+		}
+	}
+}
