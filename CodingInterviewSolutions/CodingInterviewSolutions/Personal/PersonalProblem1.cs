5cb3d5cbe97f066052b09ad5280cb260f8b11c83
Jorge Aguirre <jogonzal@microsoft.com>
2015-09-25 10:27:03 -0700

    Add personal problem 1 with unit tests

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Personal/PersonalProblem1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingInterviewSolutions.Personal
+{
+	public static class PersonalProblem1
+	{
+		/// <summary>
+		/// Given a bidimensional array of booleans, find the number of regions that are contained in that array
+		/// A region is defined by adjacent values of "true"
+		/// </summary>
+		public static int FindCountRegionsWithTrue(bool[,] matrix)
+		{
+			if (matrix == null)
+			{
+				throw new ArgumentNullException("matrix");
+			}
+
+			int rowCount = matrix.GetLength(0);
+			int columnCount = matrix.GetLength(1);
+
+			if (!(rowCount > 0 && columnCount > 0))
+			{
+				throw new ArgumentException("Array must have at least one element!");
+			}
+
+			int totalNumberOfRegions = 0;
+			// At this point, we have a rectangular matrix of rowCount x columnCount
+			// Create a new matrix for tracking, all initialized to false
+			bool[,] myBooleanMatrixForTracking = new bool[rowCount,columnCount];
+
+			for (int r = 0; r < rowCount; r++)
+			{
+				for (int c = 0; c < columnCount; c++)
+				{
+					if (myBooleanMatrixForTracking[r, c])
+					{
+						// If we've already visited this cell, continue
+						continue;
+					}
+
+					// We haven't visited this cell. If it's true, increment the number of count in the
+					// total regions and explore all adjacent "true" regions
+					// NOTE: There is a recursive solution here, but we're opting for an iterative solution, since that
+					// is more efficient, uses less memory and is in general more performant
+					if (matrix[r, c])
+					{
+						totalNumberOfRegions++;
+						Stack<Tuple<int, int>> pendingCoordinatesToExplore = new Stack<Tuple<int, int>>();
+						AddAdjacentValues(pendingCoordinatesToExplore, r, c, rowCount, columnCount);
+						myBooleanMatrixForTracking[r, c] = true;
+
+						while (pendingCoordinatesToExplore.Count != 0)
+						{
+							// Pop from the stack
+							Tuple<int, int> coordinatesToExplore = pendingCoordinatesToExplore.Pop();
+
+							// If explored already, skip
+							if (myBooleanMatrixForTracking[coordinatesToExplore.Item1, coordinatesToExplore.Item2])
+							{
+								// If we've already visited this cell, continue
+								continue;
+							}
+
+							// Mark as explored
+							myBooleanMatrixForTracking[coordinatesToExplore.Item1, coordinatesToExplore.Item2] = true;
+
+							// If it is true, add adjacent coordinates to the stack
+							if (matrix[coordinatesToExplore.Item1, coordinatesToExplore.Item2])
+							{	
+								AddAdjacentValues(pendingCoordinatesToExplore, coordinatesToExplore.Item1, coordinatesToExplore.Item2, rowCount, columnCount);
+							}
+						}
+					}
+				}
+			}
+
+			return totalNumberOfRegions;
+		}
+
+		private static void AddAdjacentValues(Stack<Tuple<int, int>> stack, int r, int c, int rowCount, int columnCount)
+		{
+			// Add upper
+			AddIfValid(stack, r - 1, c, rowCount, columnCount);
+			// Add lower
+			AddIfValid(stack, r + 1, c, rowCount, columnCount);
+			// Add left
+			AddIfValid(stack, r, c - 1, rowCount, columnCount);
+			// Add right
+			AddIfValid(stack, r, c + 1, rowCount, columnCount);
+		}
+
+		private static void AddIfValid(Stack<Tuple<int, int>> stack, int r, int c, int rowCount, int columnCount)
+		{
+			if (IsCoordinateValid(r, c, rowCount, columnCount))
+			{
+				stack.Push(new Tuple<int, int>(r, c));
+			}
+		}
+
+		private static bool IsCoordinateValid(int r, int c, int rowCount, int columnCount)
+		{
+			return (r >= 0 && r < rowCount && c >= 0 && c < columnCount);
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Personal/PersonalProblem1_UnitTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using CodingInterviewSolutions.Personal;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Personal
+{
+	[TestClass]
+	public class PersonalProblem1_UnitTests
+	{
+		[TestMethod]
+		public void MatrixWithNoRegions()
+		{
+			bool[,] matrix = new bool[3, 3]
+			{
+				{false, false, false},
+ 				{false, false, false},
+				{false, false, false}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(0);
+		}
+
+		[TestMethod]
+		public void MatrixWith1RegionWith1()
+		{
+			bool[,] matrix = new bool[3, 3]
+			{
+				{false, false, false},
+ 				{false, true, false},
+				{false, false, false}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(1);
+		}
+
+		[TestMethod]
+		public void MatrixWith2RegionsWith1()
+		{
+			bool[,] matrix = new bool[3, 3]
+			{
+				{false, false, false},
+ 				{false, true, false},
+				{false, false, true}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(2);
+		}
+
+		[TestMethod]
+		public void MatrixWith3RegionsWith1()
+		{
+			bool[,] matrix = new bool[3, 3]
+			{
+				{true, false, false},
+ 				{false, true, false},
+				{false, false, true}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(3);
+		}
+
+		[TestMethod]
+		public void MatrixWith1RegionWith6()
+		{
+			bool[,] matrix = new bool[3, 5]
+			{
+				{false, false, false, true, true},
+ 				{false, false, false, true, true},
+				{false, false, false, true, true}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(1);
+		}
+
+		[TestMethod]
+		public void MatrixWith1RegionWith61RegionWith1()
+		{
+			bool[,] matrix = new bool[3, 5]
+			{
+				{true, false, false, true, true},
+ 				{false, false, false, true, true},
+				{false, false, false, true, true}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(2);
+		}
+
+		[TestMethod]
+		public void WholeMatrixWith1Region()
+		{
+			bool[,] matrix = new bool[3, 5]
+			{
+				{true, true, true, true, true},
+ 				{true, true, true, true, true},
+				{true, true, true, true, true}
+			};
+
+			PersonalProblem1.FindCountRegionsWithTrue(matrix)
+				.Should().Be(1);
+		}
+
+		[TestMethod]
+		public void MatrixThatIsNull_Throws()
+		{
+			bool[,] matrix = null;
+
+			Action action = () =>
+			{
+				int someInt = PersonalProblem1.FindCountRegionsWithTrue(matrix);
+			};
+
+			action.ShouldThrow<ArgumentNullException>();
+		}
+	}
+}
