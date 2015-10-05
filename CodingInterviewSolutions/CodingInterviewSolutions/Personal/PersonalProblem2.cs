546ba7575ca0818ce032826902d7273b382529b9
Jorge <jogonzal@microsoft.com>
2015-10-04 20:54:06 -0700

    Add personal problem 2

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Personal/PersonalProblem2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewSolutions.Personal
+{
+	/// <summary>
+	/// Find the lowest common ancestor in a binary tree
+	/// </summary>
+	public static class PersonalProblem2
+	{
+		/// <summary>
+		/// Binary tree implementation
+		/// </summary>
+		internal class Tree
+		{
+			internal Tree Left { get; set; }
+			internal Tree Right { get; set; }
+			internal int Value { get; set; }
+		}
+
+		internal static Tree LowestCommonAncestor(Tree root , int value1, int value2)
+		{
+			int smallValue = Math.Min(value1, value2);
+			int bigValue = Math.Max(value1, value2);
+
+			return LowestCommonAncestorPrivate(root, smallValue, bigValue);
+		}
+
+		private static Tree LowestCommonAncestorPrivate(Tree root, int smallValue, int bigValue)
+		{
+			if (root == null)
+			{
+				return null;
+			}
+
+			if (root.Value < smallValue && root.Value < bigValue)
+			{
+				// Both are going right
+				return LowestCommonAncestorPrivate(root.Right, smallValue, bigValue);
+			}
+			else if (root.Value > smallValue && root.Value > bigValue)
+			{
+				// Both are going left
+				return LowestCommonAncestorPrivate(root.Left, smallValue, bigValue);
+			}
+			else
+			{
+				// This is the intersection!
+				if (ContainsValue(root, smallValue) && ContainsValue(root, bigValue))
+				{
+					return root;
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+
+		private static bool ContainsValue(Tree root, int value)
+		{
+			if (root == null)
+			{
+				return false;
+			}
+
+			if (root.Value == value)
+			{
+				return true;
+			}
+			else if (value > root.Value)
+			{
+				return ContainsValue(root.Right, value);
+			}
+			else
+			{
+				return ContainsValue(root.Left, value);
+			}
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Personal/PersonalProblem2_UnitTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingInterviewSolutions.Personal;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Personal
+{
+	[TestClass]
+	public class PersonalProblem2_UnitTests
+	{
+		private static PersonalProblem2.Tree _sampleTree = new PersonalProblem2.Tree()
+		{
+			Value = 5,
+			Left = new PersonalProblem2.Tree()
+			{
+				Value = 2,
+				Right = new PersonalProblem2.Tree()
+				{
+					Value = 3,
+				},
+				Left = new PersonalProblem2.Tree()
+				{
+					Value = 1
+				}
+			},
+			Right = new PersonalProblem2.Tree()
+			{
+				Value = 20,
+				Right = new PersonalProblem2.Tree()
+				{
+					Value = 40,
+					Left = new PersonalProblem2.Tree()
+					{
+						Value = 30
+					},
+					Right = new PersonalProblem2.Tree()
+					{
+						Value = 50
+					}
+				},
+				Left = new PersonalProblem2.Tree()
+				{
+					Value = 15
+				}
+			}
+		};
+
+		[TestMethod]
+		public void Null_Null()
+		{
+			PersonalProblem2.Tree tree = null;
+			PersonalProblem2.LowestCommonAncestor(tree, 1, 2).Should().BeNull();
+		}
+
+		[TestMethod]
+		public void SampleTree_2()
+		{
+			PersonalProblem2.LowestCommonAncestor(_sampleTree, 3, 1).Value.Should().Be(2);
+		}
+
+		[TestMethod]
+		public void SampleTree_LEafElements()
+		{
+			PersonalProblem2.LowestCommonAncestor(_sampleTree, 30, 50).Value.Should().Be(40);
+		}
+
+		[TestMethod]
+		public void SampleTree_AllTree()
+		{
+			PersonalProblem2.LowestCommonAncestor(_sampleTree, 50, 1).Value.Should().Be(5);
+		}
+
+		[TestMethod]
+		public void SampleTree_ElementDoesNotExist()
+		{
+			PersonalProblem2.LowestCommonAncestor(_sampleTree, 0, 1).Should().BeNull();
+		}
+	}
+}
