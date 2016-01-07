a57bc98b2c4f82050ad0174294ce89667c6e438b
Jorge Aguirre <jogonzal@microsoft.com>
2016-01-06 18:50:49 -0800

    Add SmallProblem2

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Small/SmallProblem2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewSolutions.Small
+{
+	/// <summary>
+	/// Given a string (as an array of characters) replace all spaces with "%20"
+	/// </summary>
+	public static class SmallProblem2
+	{
+		public static char[] EncodeSpaces(char[] input)
+		{
+			if (input == null || input.Length == 0)
+			{
+				// Nothing to do here
+				return input;
+			}
+
+			// Count the spaces
+			int numberOfSpaces = 0;
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == ' ')
+				{
+					numberOfSpaces++;
+				}
+			}
+
+			if (numberOfSpaces == 0)
+			{
+				return input;
+			}
+
+			char[] output = new char[input.Length + numberOfSpaces * 2];
+			int outputOffset = 0;
+			for (int inputOffset = 0; inputOffset < input.Length; inputOffset++)
+			{
+				if (input[inputOffset] == ' ')
+				{
+					output[outputOffset++] = '%';
+					output[outputOffset++] = '2';
+					output[outputOffset++] = '0';
+				}
+				else
+				{
+					output[outputOffset++] = input[inputOffset];
+				}
+			}
+
+			return output;
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Small/SmallProblem2UnitTests.cs
@@ -0,0 +1,45 @@
+﻿using CodingInterviewSolutions.Small;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewSolutionsUnitTests.Small
+{
+	[TestClass]
+	public class SmallProblem2UnitTests
+	{
+		[TestMethod]
+		public void SmallProblem2Tests()
+		{
+			TestSmallProblem2(
+				new char[] { 'h', 'o', 'l', 'a' },
+				new char[] { 'h', 'o', 'l', 'a' });
+
+			TestSmallProblem2(
+				new char[] { 'h', 'o', ' ', 'a' },
+				new char[] { 'h', 'o', '%', '2', '0', 'a' });
+
+			TestSmallProblem2(
+				new char[] { ' ' },
+				new char[] { '%', '2', '0' });
+
+			TestSmallProblem2(
+				new char[] { 'h', 'o', ' ', 'a', ' ' },
+				new char[] { 'h', 'o', '%', '2', '0', 'a', '%', '2', '0' });
+
+		}
+
+		private static void TestSmallProblem2(char[] input, char[] expectedOutput)
+		{
+			char[] output = SmallProblem2.EncodeSpaces(input);
+			output.Should().BeEquivalentTo(expectedOutput);
+		}
+	}
+}
