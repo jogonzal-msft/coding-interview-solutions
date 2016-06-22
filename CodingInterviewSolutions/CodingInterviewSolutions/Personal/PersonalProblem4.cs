ab2f4d8d341ed2284256dfd2656aa4df8ab2d510
Jorge <jogonzal@microsoft.com>
2016-06-22 14:40:40 -0700

    Stub for new problem - before I forget!

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Personal/PersonalProblem4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewSolutions.Personal
+{
+	public class PersonalProblem4
+	{
+		public class Contact
+		{
+			public string Name { get; set; }
+			public List<string> Emails { get; set; }
+		}
+
+		public class CondensedContact
+		{
+			public List<string> Names { get; set; }
+			public List<string> Emails { get; set; }
+		}
+
+		public static List<CondensedContact> Solve(List<Contact> contacts)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Personal/PersonalProblem4_UnitTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using CodingInterviewSolutions.Personal;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Personal
+{
+	[TestClass]
+	public class PersonalProblem4_UnitTests
+	{
+		[TestMethod]
+		public void MultipleLists_OneList()
+		{
+			var contacts = new List<PersonalProblem4.Contact>()
+			{
+				new PersonalProblem4.Contact()
+				{
+					Name = "Jorge",
+					Emails = new List<string>()
+					{
+						"jorge@msft.com",
+						"jo@msft.com",
+					}
+				},
+				new PersonalProblem4.Contact()
+				{
+					Name = "George",
+					Emails = new List<string>()
+					{
+						"jo@msft.com",
+						"jorgeaguirre@msft.com",
+					}
+				},
+				new PersonalProblem4.Contact()
+				{
+					Name = "Bob",
+					Emails = new List<string>()
+					{
+						"bob@msft.com",
+						"bob2@msft.com",
+					}
+				},
+				new PersonalProblem4.Contact()
+				{
+					Name = "Jorge2",
+					Emails = new List<string>()
+					{
+						"jrogenew@msft.com",
+						"jorge@msft.com",
+					}
+				},
+			};
+
+			var expected = new List<PersonalProblem4.CondensedContact>()
+			{
+				new PersonalProblem4.CondensedContact()
+				{
+					Names = new List<string>()
+					{
+						"Jorge",
+						"Jorge2",
+						"George",
+					},
+					Emails = new List<string>()
+					{
+						"jorge@msft.com",
+						"jo@msft.com",
+						"jrogenew@msft.com",
+						"jorgeaguirre@msft.com",
+					}
+				},
+				new PersonalProblem4.CondensedContact()
+				{
+					Names = new List<string>()
+					{
+						"Bob",
+					},
+					Emails = new List<string>()
+					{
+						"bob@msft.com",
+						"bob2@msft.com",
+					}
+				},
+			};
+
+			List<PersonalProblem4.CondensedContact> result = PersonalProblem4.Solve(contacts);
+
+			result.Should().BeEquivalentTo(expected);
+		}
+	}
+}
