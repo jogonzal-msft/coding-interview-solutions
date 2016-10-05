2e63eb2943b5ef701ec4764c5a050f6e0f747b38
Jorge Aguirre <jogonzal@microsoft.com>
2016-10-05 11:41:48 -0700

    Add "print deepest path in bonary tree" problem

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/DeepestPathInBinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingInterviewSolutions.Named
+{
+	public static class DeepestPathInBinaryTree
+	{
+		/// <summary>
+		/// Binary tree implementation
+		/// </summary>
+		internal class Tree
+		{
+			internal Tree Left { get; set; }
+			internal Tree Right { get; set; }
+			internal int Value { get; set; }
+		}
+
+		internal static List<Tree> Run(Tree root)
+		{
+			// Questions to ask here
+			// 1. Is the tree ordered?
+			// If it is, there is a simple O(2 * log(n)) solution
+			// 2. If not, then this is the solution that is O(n)
+
+			if (root == null)
+			{
+				throw new ArgumentException("Root should not be null");
+			}
+
+			List<Tree> currentPath = new List<Tree>();
+			List<Tree> deepestPath = new List<Tree>();
+
+			FindDeepestPath(root, currentPath, ref deepestPath);
+
+			return deepestPath;
+		}
+
+		private static void FindDeepestPath(Tree currentNode, List<Tree> currentPath, ref List<Tree> deepestPath)
+		{
+			// Calculate current path
+			currentPath = new List<Tree>(currentPath) {currentNode};
+
+			// Go left and right (if possible)
+			if (currentNode.Left != null)
+			{
+				FindDeepestPath(currentNode.Left, currentPath, ref deepestPath);
+			}
+			if (currentNode.Right != null)
+			{
+				FindDeepestPath(currentNode.Right, currentPath, ref deepestPath);
+			}
+
+			// Only if it's a leaf node, compare against deepest path
+			if (currentNode.Left == null && currentNode.Right == null
+				&& currentPath.Count > deepestPath.Count)
+			{
+				deepestPath = currentPath;
+			}
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/DeepestPathInBinaryTreeUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CodingInterviewSolutions.Named;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class DeepestPathInBinaryTreeUnitTests
+	{
+		private static DeepestPathInBinaryTree.Tree _sampleTree = new DeepestPathInBinaryTree.Tree()
+		{
+			Value = 5,
+			Left = new DeepestPathInBinaryTree.Tree()
+			{
+				Value = 2,
+				Right = new DeepestPathInBinaryTree.Tree()
+				{
+					Value = 3,
+				},
+				Left = new DeepestPathInBinaryTree.Tree()
+				{
+					Value = 1
+				}
+			},
+			Right = new DeepestPathInBinaryTree.Tree()
+			{
+				Value = 20,
+				Right = new DeepestPathInBinaryTree.Tree()
+				{
+					Value = 40,
+					Left = new DeepestPathInBinaryTree.Tree()
+					{
+						Value = 30
+					},
+					Right = new DeepestPathInBinaryTree.Tree()
+					{
+						Value = 50,
+						Left = new DeepestPathInBinaryTree.Tree()
+						{
+							Value = 100
+						},
+					}
+				},
+				Left = new DeepestPathInBinaryTree.Tree()
+				{
+					Value = 15
+				}
+			}
+		};
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var deepestPath = DeepestPathInBinaryTree.Run(_sampleTree);
+			var values = deepestPath.Select(n => n.Value);
+			values.Should().BeEquivalentTo(new List<int>() {5, 20, 40, 50, 100});
+		}
+	}
+}
