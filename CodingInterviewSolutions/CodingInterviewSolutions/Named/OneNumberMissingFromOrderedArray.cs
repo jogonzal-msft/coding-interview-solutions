82590291b0cb761ae66da5dcbe7d0d556824a6be
Jorge Aguirre <jogonzal@microsoft.com>
2016-10-05 15:24:39 -0700

    OneNumberMissingFromOrderedArray

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/OneNumberMissingFromOrderedArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingInterviewSolutions.Named
+{
+	public static class OneNumberMissingFromOrderedArray
+	{
+		public static int Run(int[] arr)
+		{
+			HashSet<int> hash = new HashSet<int>();
+			for(int i = 0; i < arr.Length + 1; i++)
+			{
+				hash.Add(i);
+			}
+
+			foreach (var i in arr)
+			{
+				if (!InRange(i, arr.Length + 1))
+				{
+					throw new ArgumentException("outside of range!");
+				}
+
+				if (!hash.Remove(i))
+				{
+					// Number is not in hash - this should never happen
+					throw new ArgumentException("two numbers missing!");
+				}
+			}
+
+			return hash.Single();
+		}
+
+		private static bool InRange(int i, int length)
+		{
+			return (i >= 0 && i < length);
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/OneNumberMissingFromOrderedArrayUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingInterviewSolutions.Named;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class OneNumberMissingFromOrderedArrayUnitTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var input = new List<int>()
+			{
+				0,
+				1,
+				2,
+				3,
+				4,
+				5,
+				7,
+				8,
+				9
+			};
+
+			OneNumberMissingFromOrderedArray.Run(input.ToArray()).Should().Be(6);
+		}
+	}
+}
