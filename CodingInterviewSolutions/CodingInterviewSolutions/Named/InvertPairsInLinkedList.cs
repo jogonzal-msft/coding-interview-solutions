fe4af3e4027b8fa93f2c1b8ee03ccd4b4cd37f47
Jorge Aguirre <jogonzal@microsoft.com>
2016-10-09 23:44:51 -0700

    Add linked list pair inversion

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/InvertPairsInLinkedList.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace CodingInterviewSolutions.Named
+{
+	public static class InvertPairsInLinkedList
+	{
+		public class Node
+		{
+			public int Value { get; set; }
+			public Node Next { get; set; }
+
+			public override string ToString()
+			{
+				StringBuilder sb = new StringBuilder();
+				Node current = this;
+				while (current != null)
+				{
+					sb.Append(current.Value + ",");
+					current = current.Next;
+				}
+				return sb.ToString();
+			}
+		}
+
+
+		/// <summary>
+		/// The first node and the last one are special (in c++ at least) because the first one has to be inverted by reference
+		/// </summary>
+		public static void Run(ref Node head)
+		{
+			Node first = head;
+
+			if (head != null && head.Next != null)
+			{
+				// Invert reference so caller also sees this
+				head = head.Next;
+			}
+
+			Node previous = null;
+
+			// Cycle through list
+			while (first != null && first.Next != null)
+			{
+				Node second = first.Next;
+				if (previous != null)
+				{
+					previous.Next = second;
+				}
+
+				Node temp = second.Next;
+				second.Next = first;
+				first.Next = temp;
+
+				// Advance
+				previous = first;
+				first = first.Next;
+			}
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/InvertPairsInLinkedListUnitTests.cs
@@ -0,0 +1,114 @@
+﻿using CodingInterviewSolutions.Named;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class InvertPairsInLinkedListUnitTests
+	{
+		[TestMethod]
+		public void ListWith4Elements()
+		{
+			var input = new InvertPairsInLinkedList.Node()
+			{
+				Value = 1,
+				Next = new InvertPairsInLinkedList.Node()
+				{
+					Value = 2,
+					Next = new InvertPairsInLinkedList.Node()
+					{
+						Value = 3,
+						Next = new InvertPairsInLinkedList.Node()
+						{
+							Value = 4
+						}
+					}
+				}
+			};
+
+			input.ToString().Should().Be("1,2,3,4,");
+
+			InvertPairsInLinkedList.Run(ref input);
+
+			input.ToString().Should().Be("2,1,4,3,");
+		}
+
+		[TestMethod]
+		public void ListWith1Element()
+		{
+			var input = new InvertPairsInLinkedList.Node()
+			{
+				Value = 1
+			};
+
+			input.ToString().Should().Be("1,");
+
+			InvertPairsInLinkedList.Run(ref input);
+
+			input.ToString().Should().Be("1,");
+		}
+
+		[TestMethod]
+		public void ListWith2Elements()
+		{
+			var input = new InvertPairsInLinkedList.Node()
+			{
+				Value = 1,
+				Next = new InvertPairsInLinkedList.Node()
+				{
+					Value = 2
+				}
+			};
+
+			input.ToString().Should().Be("1,2,");
+
+			InvertPairsInLinkedList.Run(ref input);
+
+			input.ToString().Should().Be("2,1,");
+		}
+
+		[TestMethod]
+		public void EmptyList()
+		{
+			InvertPairsInLinkedList.Node input = null;
+
+			InvertPairsInLinkedList.Run(ref input);
+
+			input.Should().BeNull();
+		}
+
+		[TestMethod]
+		public void ListWith5Elements()
+		{
+			var input = new InvertPairsInLinkedList.Node()
+			{
+				Value = 1,
+				Next = new InvertPairsInLinkedList.Node()
+				{
+					Value = 2,
+					Next = new InvertPairsInLinkedList.Node()
+					{
+						Value = 3,
+						Next = new InvertPairsInLinkedList.Node()
+						{
+							Value = 4,
+							Next = new InvertPairsInLinkedList.Node()
+							{
+								Value = 5
+							}
+						}
+					}
+				}
+			};
+
+			input.ToString().Should().Be("1,2,3,4,5,");
+
+			InvertPairsInLinkedList.Run(ref input);
+
+			input.ToString().Should().Be("2,1,4,3,5,");
+		}
+	}
+}
