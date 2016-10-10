aa18b406d4c24903c8d7cc6672ebcf41cdc7be50
Jorge Aguirre <jogonzal@microsoft.com>
2016-10-10 15:54:59 -0700

    Add the SumofTwoNumbersInArray problem

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/SumOfTwoNumbersInArray.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CodingInterviewSolutions.Named
+{
+	public class SumOfTwoNumbersInArray
+	{
+		/// <summary>
+		/// Given an array with numbers and a number target, determine if the array contains two numbers which if summed together, add up to target
+		/// </summary>
+		public static bool Run(List<int> arr, int target)
+		{
+			// Build a dictionary with all the elements in it and their occurrence count
+			Dictionary<int, int> dict = new Dictionary<int, int>(arr.Count);
+			foreach (var num in arr)
+			{
+				int currentCountOfNumber;
+				if (!dict.TryGetValue(num, out currentCountOfNumber))
+				{
+					currentCountOfNumber = 0;
+				}
+				dict[num] = currentCountOfNumber + 1;
+			}
+
+			foreach (var num in arr)
+			{
+				int complement = target - num;
+				Debug.WriteLine("{0} - {1} = {2}", target, num, complement);
+				int minimumRequirement = complement == num ? 2 : 1;
+
+				int currentCount;
+				if (!dict.TryGetValue(complement, out currentCount))
+				{
+					currentCount = 0;
+				}
+
+				if (minimumRequirement <= currentCount)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/SumOfTwoNumbersInArrayUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using CodingInterviewSolutions.Named;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class SumOfTwoNumbersInArrayUnitTests
+	{
+		[TestMethod]
+		public void SimpleList_ContainsAndNotContains()
+		{
+			var list = new List<int>()
+			{
+				1, 2, 3, 4, 5
+			};
+
+			SumOfTwoNumbersInArray.Run(list, 9).Should().BeTrue();
+			SumOfTwoNumbersInArray.Run(list, 2).Should().BeFalse();
+			SumOfTwoNumbersInArray.Run(list, 3).Should().BeTrue();
+			SumOfTwoNumbersInArray.Run(list, 6).Should().BeTrue();
+			SumOfTwoNumbersInArray.Run(list, 10).Should().BeFalse();
+		}
+
+		[TestMethod]
+		public void RepeatedNumbers_Succeeds()
+		{
+			var list = new List<int>()
+			{
+				1, 2, 2
+			};
+
+			SumOfTwoNumbersInArray.Run(list, 4).Should().BeTrue();
+			SumOfTwoNumbersInArray.Run(list, 3).Should().BeTrue();
+			SumOfTwoNumbersInArray.Run(list, 2).Should().BeFalse();
+			SumOfTwoNumbersInArray.Run(list, -1).Should().BeFalse();
+		}
+	}
+}
