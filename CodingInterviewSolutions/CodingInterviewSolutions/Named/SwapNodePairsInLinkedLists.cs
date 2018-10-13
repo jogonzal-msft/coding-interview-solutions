e9c6ed3f7692a96fa1e45b114557d27531a40c25
Jorge <jogonzal@microsoft.com>
2018-10-13 16:02:11 -0700

    Start linked list problem

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/SwapNodePairsInLinkedLists.cs
@@ -0,0 +1,32 @@
+﻿namespace CodingInterviewSolutions.Named
+{
+	/// <summary>
+	/// Given a pointer to the root
+	/// 
+	/// Pitfalls: Not updating root, not making sense of the swaps that need to happen
+	/// </summary>
+	public class SwapNodePairsInLinkedList
+	{
+		public class LinkedListNode
+		{
+			public int Value;
+			public LinkedListNode Next;
+
+			public LinkedListNode(int value)
+			{
+				Value = value;
+			}
+		}
+
+		public static void SwapPairs(ref LinkedListNode root)
+		{
+			if (root?.Next == null)
+			{
+				// Nothing to swap
+				return;
+			}
+
+
+		}
+	}
+}
