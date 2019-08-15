543ab234e8ed651b0ced8b2c7a686077a21c47e5
Jorge <jogonzal@users.noreply.github.com>
2019-08-14 20:12:50 -0700

    Add the compare build #'s problem

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/CompareBuildNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewSolutions.Named
+{
+	public static class CompareBuildNumbers
+	{
+		public static int Compare(string a, string b)
+		{
+			if (a == null)
+			{
+				a = "";
+			}
+
+			if (b == null)
+			{
+				b = "";
+			}
+
+			var aChunks = a.Split('.').ToList();
+			var bChunks = b.Split('.').ToList();
+
+			// To compare, we have to "even" out the number of elements
+			while (aChunks.Count < bChunks.Count)
+			{
+				aChunks.Add("0");
+			}
+			while (bChunks.Count < aChunks.Count)
+			{
+				bChunks.Add("0");
+			}
+
+			// After they have the same # of elements, now we can start to compare, starting with the leftmost
+			for(int i = 0; i < aChunks.Count; i++)
+			{
+				var aChunk = aChunks[i];
+				var bChunk = bChunks[i];
+				int aChunkAsInt;
+				if (!int.TryParse(aChunk, out aChunkAsInt))
+				{
+					// Consider this value a 0
+					aChunkAsInt = 0;
+				}
+				int bChunkAsInt;
+				if (!int.TryParse(bChunk, out bChunkAsInt))
+				{
+					// Consider this value a 0
+					bChunkAsInt = 0;
+				}
+
+				if (aChunkAsInt < bChunkAsInt)
+				{
+					return -1; // A is smaller
+				}
+				if (aChunkAsInt > bChunkAsInt)
+				{
+					return 1; // B is smaller
+				}
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/CompareBuildNumberUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using CodingInterviewSolutions.Named;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class CompareBuildNumberUnitTests
+	{
+		[TestMethod]
+		public void TestEqual_3Digits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.1.1", "1.1.1"), 0);
+		}
+
+		[TestMethod]
+		public void TestEqual_2Digits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.9", "1.9"), 0);
+		}
+
+		[TestMethod]
+		public void TestGreater_DifferentDigits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.1.1", "1.1"), 1);
+		}
+
+		[TestMethod]
+		public void TestSmaller_DifferentDigits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.1", "1.1.1"), -1);
+		}
+
+		[TestMethod]
+		public void TestSmaller_SameDigits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.1.0", "1.1.1"), -1);
+		}
+
+		[TestMethod]
+		public void TestBigger_SameDigits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.1.1", "1.1.0"), 1);
+		}
+
+		[TestMethod]
+		public void TestInvalid_SameDigits()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.HELLO.0", "1.HELLOV2.0"), 0);
+		}
+
+		[TestMethod]
+		public void TestInvalid_SameDigitsThirdPosition()
+		{
+			Assert.AreEqual(CompareBuildNumbers.Compare("1.1.HELLOV@", "1.1.102983ASKDFAS"), 0);
+		}
+	}
+}
