743b90520976bdf1d52a9668a9b16b6a451b2df6
Jorge <jogonzal@users.noreply.github.com>
2021-03-02 22:33:52 -0800

    Cyclic sort solution

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/CyclicSort.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CodingInterviewSolutions.Named
+{
+	public class CyclicSort
+	{
+		public static List<int> Sort(List<int> list)
+		{
+			Debug.WriteLine($"Entire array is ", JsonConvert.SerializeObject(list));
+
+			// Unique numbers from 0-N in array, sort them
+			for (int i = 0; i < list.Count; i++)
+			{
+				while(i != list[i])
+				{
+					// i is not in the right location, send it to the right location
+					int temp = list[i];
+					Debug.WriteLine($"Swapping {list[i]} and {list[temp]}");
+					list[i] = list[temp];
+					list[temp] = temp;
+					Debug.WriteLine($"Entire array is ", JsonConvert.SerializeObject(list));
+				}
+			}
+
+			return list;
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/CyclicSortUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingInterviewSolutions.Named;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class CyclicSortTests
+	{
+		[TestMethod]
+		public void SampleArray1()
+		{
+			List<int> input = new List<int>()
+			{
+				0, 2, 3, 5, 4, 1
+			};
+
+			List<int> expectedOutput = new List<int>()
+			{
+				0, 1, 2, 3, 4, 5
+			};
+			var output = CyclicSort.Sort(input);
+			expectedOutput.ShouldBeEquivalentTo(output);
+		}
+	}
+}
