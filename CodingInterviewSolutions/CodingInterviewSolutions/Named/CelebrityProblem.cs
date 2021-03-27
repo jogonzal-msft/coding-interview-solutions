0c057b1b104b92345c243cba84632234adda49dd
Jorge <jogonzal@users.noreply.github.com>
2021-03-27 10:26:45 -0700

    Add the celebrity problem

---
+++ CodingInterviewSolutions/CodingInterviewSolutions/Named/CelebrityProblem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewSolutions.Named
+{
+	public class CelebrityProblem
+	{
+		public class Node
+		{
+			public Node(string name)
+			{
+				this.Name = name;
+				this.Knows = new List<Node>();
+			}
+
+			public string Name { get; set; }
+			public List<Node> Knows { get; set; }
+		}
+
+		public static Node FindCelebrity(List<Node> nodes)
+		{
+			// A celebrity is a person who knows everyone, but doesn't know anyone
+			// 1. Find 1 node that doesn't know anyone
+			var lonelyPeople = nodes.Where(n => n.Knows == null || n.Knows.Count == 0).ToList();
+
+			// If there's more than 1 or 0, then no celebrity
+			if (lonelyPeople.Count != 1)
+			{
+				return null;
+			}
+
+			Node potentialCelebrity = lonelyPeople[0];
+			// Now need to go through all nodes and validate they all know the potential celebrity
+			foreach (var node in nodes)
+			{
+				if (node == potentialCelebrity)
+				{
+					continue; // No need to validate anything for celebrity
+				}
+
+				if (!node.Knows.Contains(potentialCelebrity))
+				{
+					return null; // There isn't a celebrity
+				}
+			}
+
+			return potentialCelebrity;
+		}
+	}
+}
---
+++ CodingInterviewSolutions/CodingInterviewSolutionsUnitTests/Named/CelebrityProblemUnitTests.cs
@@ -0,0 +1,88 @@
+﻿using CodingInterviewSolutions.Named;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static CodingInterviewSolutions.Named.CelebrityProblem;
+
+namespace CodingInterviewSolutionsUnitTests.Named
+{
+	[TestClass]
+	public class CelebrityProblemUnitTests
+	{
+		[TestMethod]
+		public void NoCelebrity_Null()
+		{
+			var peopleGraph = new List<Node>();
+			Assert.AreEqual(null, CelebrityProblem.FindCelebrity(peopleGraph));
+		}
+
+		[TestMethod]
+		public void EveryoneKnowsEveryone_null()
+		{
+			var peopleGraph = new List<Node>();
+			var bob = new Node("Bob");
+			var jack = new Node("Jack");
+			var jill = new Node("Jill");
+			peopleGraph.Add(bob); peopleGraph.Add(jack); peopleGraph.Add(jill);
+			bob.Knows.Add(jack);
+			bob.Knows.Add(jill);
+			jack.Knows.Add(jill);
+			jack.Knows.Add(bob);
+			jill.Knows.Add(jack);
+			jill.Knows.Add(bob);
+			Assert.AreEqual(null, CelebrityProblem.FindCelebrity(peopleGraph));
+		}
+
+		[TestMethod]
+		public void NoOneKnowsAnyone_Null()
+		{
+			var peopleGraph = new List<Node>();
+			var bob = new Node("Bob");
+			var jack = new Node("Jack");
+			var jill = new Node("Jill");
+			peopleGraph.Add(bob); peopleGraph.Add(jack); peopleGraph.Add(jill);
+			Assert.AreEqual(null, CelebrityProblem.FindCelebrity(peopleGraph));
+		}
+
+		[TestMethod]
+		public void OnlyKnow1Person_Null()
+		{
+			var peopleGraph = new List<Node>();
+			var bob = new Node("Bob");
+			var jack = new Node("Jack");
+			var jill = new Node("Jill");
+			peopleGraph.Add(bob); peopleGraph.Add(jack); peopleGraph.Add(jill);
+			bob.Knows.Add(jill);
+			jack.Knows.Add(bob);
+			jill.Knows.Add(jack);
+			Assert.AreEqual(null, CelebrityProblem.FindCelebrity(peopleGraph));
+		}
+
+		[TestMethod]
+		public void CelebrityPresentBasic_Celebrity()
+		{
+			var peopleGraph = new List<Node>();
+			var bob = new Node("Bob");
+			var jack = new Node("Jack");
+			var jill = new Node("Jill");
+			peopleGraph.Add(bob); peopleGraph.Add(jack); peopleGraph.Add(jill);
+			bob.Knows.Add(jill);
+			jack.Knows.Add(jill);
+			Assert.AreEqual(jill, CelebrityProblem.FindCelebrity(peopleGraph));
+		}
+
+		[TestMethod]
+		public void CelebrityPresentOtherKnowsToo_Celebrity()
+		{
+			var peopleGraph = new List<Node>();
+			var bob = new Node("Bob");
+			var jack = new Node("Jack");
+			var jill = new Node("Jill");
+			peopleGraph.Add(bob); peopleGraph.Add(jack); peopleGraph.Add(jill);
+			bob.Knows.Add(jill);
+			bob.Knows.Add(jack);
+			jack.Knows.Add(jill);
+			jack.Knows.Add(bob);
+			Assert.AreEqual(jill, CelebrityProblem.FindCelebrity(peopleGraph));
+		}
+	}
+}
